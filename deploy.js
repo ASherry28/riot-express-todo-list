b033d044a2dffae467d8448075f7762e78caf882
A. Sherry <asherry.betheltech777@gmail.com>
2021-08-13 09:16:22 -0700

    added deployment automation

--- deploy.js
+++ deploy.js
@@ -86,7 +86,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.91.133.168',
+      host: '100.25.166.231',
       username: 'ubuntu',
       privateKey: 'rs-key.pem'
     })
